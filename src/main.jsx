ddcc779d48ce6006b3d0c95fcd4940528ed1a095
KimPuro <puro@protonmail.ch>
2024-12-28 17:30:24 +0900

    add code

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { App } from './App'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const pexel = (id) => `./images/${id}.png`
+const images = [
+    // 왼쪽
+    { position: [7, 1, 3], rotation: [0, -Math.PI / 4, 0], url: pexel(1) },
+    { position: [8, 1, -2], rotation: [0, -Math.PI / 4, 0], url: pexel(2) },
+
+    // 오른쪽
+    { position: [-6, 1, 2], rotation: [0, 0, 0], url: pexel(3) },
+    { position: [-2, 1, -6], rotation: [0, 0, 0], url: pexel(4) }
+]
+
+createRoot(document.getElementById('root')).render(<App images={images} />)
