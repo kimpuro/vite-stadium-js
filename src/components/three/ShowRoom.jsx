cfad87fb06395beb7f39d893ec465ff4c8be7b8e
KimPuro <puro@protonmail.ch>
2024-12-29 15:28:46 +0900

    헬퍼 off

--- src/components/three/ShowRoom.jsx
+++ src/components/three/ShowRoom.jsx
@@ -7,8 +7,8 @@ export default function ShowRoom() {
     const gltf = useLoader(GLTFLoader, './models/yankee_stadium_new_york_gltf/scene.gltf')
     return (
         <>
-            <gridHelper args={[100, 100]}/>
-            <axesHelper args={[100]}/>
+            {/*<gridHelper args={[100, 100]}/>*/}
+            {/*<axesHelper args={[100]}/>*/}
             {/*<ambientLight intensity={1}*/}
             {/*/>*/}
             {/*<directionalLight*/}
