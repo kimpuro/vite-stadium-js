9393411854fe153996b9c3edf72cd6cdbff22daf
KimPuro <puro@protonmail.ch>
2025-01-07 03:33:40 +0900

    필요없는 group 삭제

--- src/components/NoImages.jsx
+++ src/components/NoImages.jsx
@@ -8,11 +8,13 @@ export const NoImages = () => (
     <Canvas dpr={[1, 1.5]} camera={{fov: 70, position: [0, 10, 20]}}>
         <VideoText position={[0, 8, 0]}/>
         <color attach="background" args={['#191920']}/>
-        <group position={[0, -0.5, 0]}>
-        </group>
         <Environment preset="city"/>
         <ShowRoom/>
-        <CameraControls/>
+        <CameraControls
+            dollyToCursor={true}
+            minDistance={5}
+            maxDistance={23}
+        />
         {/*<ambientLight intensity={1} />*/}
         <directionalLight position={[0, 20, 0]} intensity={10}/>
         {/*<directionalLight position={[0, 40, 0]} intensity={10} />*/}
