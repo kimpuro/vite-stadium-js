e724e7b7cc019a602a399083e6a014aa4441c119
KimPuro <puro@protonmail.ch>
2025-01-07 03:25:03 +0900

    코드 정리

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,3 @@
-import * as THREE from 'three'
-import {useEffect, useRef, useState} from 'react'
-import {Canvas, useFrame} from '@react-three/fiber'
-import {useCursor, MeshReflectorMaterial, Image, Text, Environment, CameraControls, Html} from '@react-three/drei'
-import {useRoute, useLocation, Route} from 'wouter'
-import {easing} from 'maath'
-import getUuid from 'uuid-by-string'
-import ShowRoom from "./components/three/ShowRoom.jsx";
-import Stars from "./components/Stars.jsx";
-import VideoText from "./components/VideoText.jsx";
 import {NoImages} from "./components/NoImages.jsx";
 import useStore from "./store/useStore.js";
 import HasImage from "./components/HasImage.jsx";
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,4 @@ import App  from './App'
 import FullScreenButton from "./components/FullScreenButton.jsx";
 
 
-// createRoot(document.getElementById('root')).render(<><NoImages/></>)
 createRoot(document.getElementById('root')).render(<><App/><FullScreenButton/></>)
-// createRoot(document.getElementById('root')).render(<><App images={images} /></>)
