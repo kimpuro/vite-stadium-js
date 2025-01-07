a01b65f39336edcc9a5679902ab693a17fc46e17
KimPuro <puro@protonmail.ch>
2025-01-08 06:50:36 +0900

    비디오 변경

--- src/components/VideoText.jsx
+++ src/components/VideoText.jsx
@@ -3,7 +3,7 @@ import * as THREE from 'three'
 import { Text } from '@react-three/drei'
 
 export default function VideoText(props) {
-    const [video] = useState(() => Object.assign(document.createElement('video'), { src: '/drei.mp4', crossOrigin: 'Anonymous', loop: true, muted: true }))
+    const [video] = useState(() => Object.assign(document.createElement('video'), { src: '/ktwiz-video.mp4', crossOrigin: 'Anonymous', loop: true, muted: true }))
     useEffect(() => void video.play(), [video])
     return (
         <Text font="/Inter-Bold.woff" fontSize={3} letterSpacing={-0.06} {...props}>
