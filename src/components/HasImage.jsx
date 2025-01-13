86118248f819b1c7bbbbac54ca076f65bceb0f87
KimPuro <puro@protonmail.ch>
2025-01-14 07:47:40 +0900

    이미지 변경

--- src/components/HasImage.jsx
+++ src/components/HasImage.jsx
@@ -21,16 +21,16 @@ const ImageDescription = [
 
 export default function HasImage() {
     // height 750 width 1260
-    const imageID = (id) => `./images/${id}.webp`
+    const imageID = (id) => `./images/${id}.jpg`
 
     const images = [
         // 왼쪽
         { position: [7, 1, 3], rotation: [0, -Math.PI / 4, 0], url: imageID(1) },
-        { position: [8, 1, -2], rotation: [0, -Math.PI / 4, 0], url: imageID(2) },
+        { position: [1, 1, -6], rotation: [0, 0, 0], url: imageID(2) },
 
         // 오른쪽
         { position: [-6, 1, 2], rotation: [0, 0, 0], url: imageID(3) },
-        { position: [-2, 1, -6], rotation: [0, 0, 0], url: imageID(4) }
+        // { position: [8, 1, -2], rotation: [0, -Math.PI / 4, 0], url: imageID(4) }
     ]
     return (
         <>
@@ -123,9 +123,9 @@ function Frame({url,index, c = new THREE.Color(), ...props}) {
                 </mesh>
                 <Image raycast={() => null} ref={image} position={[0, 0, 0.7]} url={url}/>
             </mesh>
-            <Text maxWidth={0.1} anchorX="left" anchorY="top" position={[0.55, GOLDENRATIO, 0]} fontSize={0.05}>
-                {description}
-            </Text>
+            {/*<Text maxWidth={0.1} anchorX="left" anchorY="top" position={[0.55, GOLDENRATIO, 0]} fontSize={0.05}>*/}
+            {/*    {description}*/}
+            {/*</Text>*/}
         </group>
     )
 }
